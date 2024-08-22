eb9f1d78451fdfc9e4f6ed772535cf889a836f95
92S-gaitan <sabrina.gaitan@sancorsalud.com.ar>
2024-08-21 23:07:17 -0300

    Rest Assured

--- src/test/java/com/lamansys/runner/TestRunner.java
+++ src/test/java/com/lamansys/runner/TestRunner.java
@@ -6,7 +6,6 @@ import io.cucumber.testng.CucumberOptions;
 @CucumberOptions(
         features = "src/test/resources/features",
         glue = {"com.lamansys.steps"},
-        tags = "@OBTENER-PRIMER-PRODUCTO",
         plugin = {"pretty",
                 "json:target/cucumber-reports/cucumber.json",
                 "html:target/cucumber-reports/cucumberreport.html"}
